90f0bdfdf51b6d90ada2e31a2389a4b921cba535
alordash <54469554+alordash@users.noreply.github.com>
2020-08-19 18:41:35 +0300

    Added date-cases.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { parseDate, extractWords } = require('./lib/date-parser');
+const { extractDates } = require('./lib/date-cases');
+extractDates({text: "lolol"});
 
 let obj = extractWords("6 апреля в 15.00 На Ногти Мне");
 let str = '';
