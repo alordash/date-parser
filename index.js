e778146f759d8eca10fb795b4f35f2553a8a4350
alordash <54469554+alordash@users.noreply.github.com>
2020-08-19 22:15:24 +0300

    Added basic parsing cases

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { parseDate, extractWords } = require('./lib/date-parser');
-const { extractDates } = require('./lib/date-cases');
-extractDates({text: "lolol"});
 
-let obj = extractWords("6 апреля в 15.00 На Ногти Мне");
+let original = "6 апреля 9 марта в 15.00 за водой 40 секунд 20 минут 50 августа 45 года 2010 года";
+let obj = extractWords(original);
 let str = '';
 obj.forEach(v => str += v.regex_char);
 console.log(str);
+console.log('parseDate(original) :>> ', parseDate(original));
 console.log("exiting . . .");
