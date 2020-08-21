d4d29eded4d16bd68e3aa5bc8d47087c1e3fc0ab
alordash <54469554+alordash@users.noreply.github.com>
2020-08-21 21:40:23 +0300

    Changed TimeList(array properties) to Array.<TimeList>

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { parseDate, extractWords } = require('./lib/date-parser');
 
-let original = "6 апреля на 9 марта в 15.00 за водой в 40 секунд 20 минут 50 августа 45 года 2010 года";
+//let original = "6 апреля на 9 марта в 15.00 за водой в 40 секунд 20 минут 50 августа 45 года 2010 года";
+let original = "6 апреля на 9 марта за водой в 40 секунд 20 минут 50 августа 45 года 2010 года";
 let obj = extractWords(original);
 let str = '';
 obj.forEach(v => str += v.regex_char);
 console.log(str);
-console.log('parseDate(original) :>> ', parseDate(original));
+let resDate = parseDate(original);
+console.log('resDate :>> ', resDate);
 console.log("exiting . . .");
