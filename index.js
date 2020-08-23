5f2494c54badc3e77cc4919b51fb739a3e06568c
alordash <54469554+alordash@users.noreply.github.com>
2020-08-23 11:16:43 +0300

    Added "I" regchar that separates sentence

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { parseDate, extractWords } = require('./lib/date-parser');
 
 //let original = "6 апреля на 9 марта в 15.00 за водой в 40 секунд 20 минут 50 августа 45 года 2010 года";
-let original = "6 апреля в 15:59 на 9 марта за водой в 40 секунд 20 минут 6 часов 50 августа 45 года 2010 года";
+let original = "6 апреля в 15:59, на 9 марта за водой в 40 секунд 20 минут 6 часов 50 августа 45 года и 2010 года";
 let obj = extractWords(original);
 let str = '';
 obj.forEach(v => str += v.regex_char);
