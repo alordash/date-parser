8532b4c526c66bff4dced765cedd54fe65891a3e
alordash <54469554+alordash@users.noreply.github.com>
2020-08-29 22:53:09 +0300

    Renamed extractWords to extractRegexChars

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { parseDate, extractWords } = require('./lib/date-parser');
+const { parseDate, extractRegexChars } = require('./lib/date-parser');
 
-const original = "something at 6 o'clock pm";
+const original = "come home in 10 seconds 20 minutes 30 hours. Buy milk and wash car on monday Wash my car ";
 
-let obj = extractWords(original);
+let obj = extractRegexChars(original);
 let str = '';
 obj.forEach(v => str += v.regex_char);
 console.log(str);
 let resDate = parseDate(original, 1, 50);
 console.log('resDate :>> ', resDate);
-console.log("exiting . . .");
+rconsole.log("exiting . . .");
