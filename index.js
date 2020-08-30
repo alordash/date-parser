2da62213def1e0d7e2923563d137e71faa8160d0
alordash <54469554+alordash@users.noreply.github.com>
2020-08-30 21:41:56 +0300

    module.exports

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const { parseDate, extractRegexChars } = require('./lib/date-parser');
-
-const original = "come home in 10 seconds 20 minutes 30 hours. Buy milk and wash car on monday Wash my car ";
-
-let obj = extractRegexChars(original);
-let str = '';
-obj.forEach(v => str += v.regex_char);
-console.log(str);
-let resDate = parseDate(original, 1, 50);
-console.log('resDate :>> ', resDate);
-console.log("exiting . . .");
+module.exports = require('./lib/date-parser');
